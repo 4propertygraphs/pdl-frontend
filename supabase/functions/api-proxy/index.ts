d69d28ba09539d6a752440aab89ab0364649686c
Frank-NikoAI <frank@4property.com>
2025-10-07 10:47:10 +0100

    Updated index.ts

--- supabase/functions/api-proxy/index.ts
+++ supabase/functions/api-proxy/index.ts
@@ -1,7 +1,7 @@
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Client-Info, Apikey, token",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Client-Info, Apikey, token, key",
 };
 
 Deno.serve(async (req: Request) => {
